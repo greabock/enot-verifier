0a45f59d0fad9dc26228b68ded810903e1fc1546
Роман Сохарев <greabock@gmail.com>
2022-06-07 08:12:04 +0700

    remove unused import

--- src/Contracts/Notifier.php
+++ src/Contracts/Notifier.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Enot\Verifier\Contracts;
 
-use Enot\Verifier\Exception\UnknownNotifier;
-
 interface Notifier
 {
     public function notify(Verification $verification);
